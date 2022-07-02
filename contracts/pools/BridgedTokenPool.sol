c2a68dad73440d2046eb906d246ac28fc58e2f00
Eddie Penta <eddie@avalabs.org>
2022-07-02 18:52:49 -0400

    use diamond storage to make contract upgradable

--- contracts/pools/BridgedTokenPool.sol
+++ contracts/pools/BridgedTokenPool.sol
@@ -5,13 +5,28 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IL1ERC20Bridge} from "@eth-optimism/contracts/L1/messaging/IL1ERC20Bridge.sol";
 
 contract BridgedTokenPool is TokenPool, Ownable {
-    IL1ERC20Bridge public bridge;
-    address public l2Pool;
-    address public l2Token;
-    uint32 private l2GasLimit;
+    bytes32 constant L2_DATA_SLOT =
+        keccak256("com.warriders.token.pool.bridge");
+
+    struct L2Data {
+        address bridge;
+        address l2Pool;
+        address l2Token;
+        uint32 l2GasLimit;
+    }
 
     constructor(address standardBridge) {
-        bridge = IL1ERC20Bridge(standardBridge);
+        _l2Data().bridge = standardBridge;
+    }
+
+    /**
+     * @dev The ProxyData struct stored in this registered Extension instance.
+     */
+    function _l2Data() internal pure returns (L2Data storage ds) {
+        bytes32 position = L2_DATA_SLOT;
+        assembly {
+            ds.slot := position
+        }
     }
 
     function setL2(
@@ -19,21 +34,27 @@ contract BridgedTokenPool is TokenPool, Ownable {
         address _l2Pool,
         uint32 _l2GasLimit
     ) external onlyOwner {
-        require(l2Pool == address(0), "L2 Already set");
-        l2Pool = _l2Pool;
-        l2Token = _l2Token;
-        l2GasLimit = _l2GasLimit;
+        L2Data storage data = _l2Data();
+
+        require(data.l2Pool == address(0), "L2 Already set");
+        data.l2Pool = _l2Pool;
+        data.l2Token = _l2Token;
+        data.l2GasLimit = _l2GasLimit;
     }
 
     function bridgeTokens(uint256 tokenAmount) external onlyOwner {
-        require(l2Pool != address(0), "L2 data not set");
+        L2Data storage data = _l2Data();
 
-        bridge.depositERC20To(
+        require(data.l2Pool != address(0), "L2 data not set");
+
+        IL1ERC20Bridge tokenBridge = IL1ERC20Bridge(data.bridge);
+
+        tokenBridge.depositERC20To(
             address(token),
-            l2Token,
-            l2Pool,
+            data.l2Token,
+            data.l2Pool,
             tokenAmount,
-            l2GasLimit,
+            data.l2GasLimit,
             ""
         );
     }
