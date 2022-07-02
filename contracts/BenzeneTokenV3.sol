e57e3b70733c80039f67b1e59834f40a110300f2
Eddie Penta <eddie@avalabs.org>
2022-07-02 14:19:21 -0400

    upgrade BZN to 3.0

---
+++ contracts/BenzeneTokenV3.sol
@@ -0,0 +1,84 @@
+pragma solidity >=0.7.6 <=0.8.9;
+
+import {MigratedBenzeneToken} from "./MigratedBenzeneToken.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract BenzeneTokenV3 is MigratedBenzeneToken, ERC20Permit, Ownable {
+    mapping(address => bool) public addressAllowedMigration;
+    mapping(address => bool) public addressBlocked;
+
+    constructor(
+        address gamePool,
+        address teamPool, //vest
+        address advisorPool,
+        address oldTeamPool,
+        address oldAdvisorPool,
+        address[] memory oldBzn
+    )
+        ERC20Permit(TokenName)
+        MigratedBenzeneToken(
+            gamePool,
+            teamPool,
+            advisorPool,
+            oldTeamPool,
+            oldAdvisorPool,
+            oldBzn
+        )
+    {}
+
+    function _migrate(
+        address token,
+        address account,
+        uint256 amount
+    ) internal override {
+        require(
+            addressAllowedMigration[account],
+            "Address is not allowed to migrate tokens"
+        );
+        super._migrate(token, account, amount);
+    }
+
+    function _migrateAll(address token, address account) internal override {
+        require(
+            addressAllowedMigration[account],
+            "Address is not allowed to migrate tokens"
+        );
+        super._migrateAll(token, account);
+    }
+
+    function toggleTokenMigrationAccess(address account, bool access)
+        external
+        onlyOwner
+    {
+        addressAllowedMigration[account] = access;
+    }
+
+    function batchToggleTokenMigrationAccess(
+        address[] calldata accounts,
+        bool access
+    ) external onlyOwner {
+        for (uint256 i = 0; i < accounts.length; i++) {
+            addressAllowedMigration[accounts[i]] = access;
+        }
+    }
+
+    function blocklistAddress(address account) external onlyOwner {
+        addressBlocked[account] = true;
+    }
+
+    function removeBlocklistedAddress(address account) external onlyOwner {
+        addressBlocked[account] = false;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        bool fromAllowed = from == address(0) || !addressBlocked[from];
+        bool toAllowed = to == address(0) || !addressBlocked[to];
+
+        require(fromAllowed && toAllowed, "Address block listed");
+    }
+}
--- contracts/TokenUpdate.sol
+++ contracts/TokenUpdate.sol
@@ -1,57 +1,64 @@
-pragma solidity >=0.7.6<=0.8.9;
+pragma solidity >=0.7.6 <=0.8.9;
 
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import "./base/ITokenMigratable.sol";
 
 abstract contract TokenUpdate is ERC20Burnable, ITokenMigratable {
-    
     mapping(address => bool) internal _legacyTokens;
-    
+
     address internal defaultLegacyToken;
 
-    function _migrate(address token, address account, uint256 amount) internal {
-       require(_legacyTokens[token]);
-       
-       ERC20Burnable legacyToken = ERC20Burnable(token);
-       
-       legacyToken.burnFrom(account, amount);
-       _mint(account, amount); 
+    function _migrate(
+        address token,
+        address account,
+        uint256 amount
+    ) internal virtual {
+        require(_legacyTokens[token]);
+
+        ERC20Burnable legacyToken = ERC20Burnable(token);
+
+        legacyToken.burnFrom(account, amount);
+        _mint(account, amount);
     }
 
-    function _migrateAll(address token, address account) internal {
-      require(_legacyTokens[token]);
-       
-      IERC20 legacyToken = IERC20(token);
-       
-      uint256 balance = legacyToken.balanceOf(account);
-      uint256 allowance = legacyToken.allowance(account, address(this));
-      uint256 amount = Math.min(balance, allowance);
-      _migrate(token, account, amount);
+    function _migrateAll(address token, address account) internal virtual {
+        require(_legacyTokens[token]);
+
+        IERC20 legacyToken = IERC20(token);
+
+        uint256 balance = legacyToken.balanceOf(account);
+        uint256 allowance = legacyToken.allowance(account, address(this));
+        uint256 amount = Math.min(balance, allowance);
+        _migrate(token, account, amount);
     }
-                
+
     /**
-   * @dev Transfers part of an account's balance in the old token to this
-   * contract, and mints the same amount of new tokens for that account.
-   * @param token The legacy token to migrate from, should be registered under this token
-   * @param account whose tokens will be migrated
-   * @param amount amount of tokens to be migrated
-   */
-   function migrate(address token, address account, uint256 amount) external override {
-       _migrate(token, account, amount);
-   }
+     * @dev Transfers part of an account's balance in the old token to this
+     * contract, and mints the same amount of new tokens for that account.
+     * @param token The legacy token to migrate from, should be registered under this token
+     * @param account whose tokens will be migrated
+     * @param amount amount of tokens to be migrated
+     */
+    function migrate(
+        address token,
+        address account,
+        uint256 amount
+    ) external override {
+        _migrate(token, account, amount);
+    }
 
-  /**
-   * @dev Transfers all of an account's allowed balance in the old token to
-   * this contract, and mints the same amount of new tokens for that account.
-   * @param token The legacy token to migrate from, should be registered under this token
-   * @param account whose tokens will be migrated
-   */
-  function migrateAll(address token, address account) external override {
-      _migrateAll(token, account);
-  }
-  
-  function migrateAll(address account) external override {
-      _migrateAll(defaultLegacyToken, account);
-  }
+    /**
+     * @dev Transfers all of an account's allowed balance in the old token to
+     * this contract, and mints the same amount of new tokens for that account.
+     * @param token The legacy token to migrate from, should be registered under this token
+     * @param account whose tokens will be migrated
+     */
+    function migrateAll(address token, address account) external override {
+        _migrateAll(token, account);
+    }
+
+    function migrateAll(address account) external override {
+        _migrateAll(defaultLegacyToken, account);
+    }
 }
